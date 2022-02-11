53a502f59253cd7b805cab61c0daeb41dc989469
coa3 <98691358+coa3@users.noreply.github.com>
2022-02-11 19:20:44 +0300

    Add files via upload

---
+++ yrok1.js
@@ -0,0 +1,19 @@
+//console.log("oleg")
+//console.log(3);
+//console.log("Helo" + " " + "wrd");
+//console.info("HashChangeEvent");
+//document.querySelector("h2").innerHTML = 5;
+//console.log("Добро" + " " + "пожаловать " + " " + "на курс");
+//alert("2019");
+//console.log(2019 - 200);
+//document.getElementById("six").innerHTML = "Hello World";
+
+//document.getElementById("seven").innerHTML = 12 * 12;
+//document.querySelector("#out-8").innerHTML = "task-8";
+//document.querySelector("#out-10").innerHTML = "<h2>Hi</h2>";
+//document.querySelector("#out-11").innerHTML = 123;
+//document.querySelector("#out-11").innerHTML += 456;
+//let a = document.querySelector("#out-12");
+//a.innerHTML = 3.145;
+//let out13 = document.querySelector("#out-13");
+//out13.innerHTML = '<img src="https://itgid.info/img/js20/js20_unit_01.png" alt="js">';
