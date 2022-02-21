f0f6c14e28c4873d911caeac4609998dc4ff8e9c
coa3 <98691358+coa3@users.noreply.github.com>
2022-02-21 20:45:36 +0300

    Add files via upload

--- freecodecamp.js
+++ freecodecamp.js
@@ -9,3 +9,44 @@ const myArray = [
     ["cat", 2]
 ];
 let removedFromMyArray = myArray.pop();
+const myArray = [
+    ["John", 23],
+    ["dog", 3]
+];
+let removedFromMyArray = myArray.shift();
+const myArray = [
+    ["John", 23],
+    ["dog", 3]
+];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+
+function reusableFunction() {
+    console.log('Hi World');
+}
+reusableFunction(); //вызов функции
+
+function functionWithArgs(one, two) {
+    console.log(one + two);
+}
+functionWithArgs(1, 2);
+
+function timesFive(num) {
+    return num * 5;
+}
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+
+    var outerWear = 'sweater';
+
+    return outerWear;
+}
+myOutfit(); //переопределение функции
+
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+processed = processArg(7);
