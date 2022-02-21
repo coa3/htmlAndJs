a563bdf1ecc23ce9bf68a994b2a7fdd5ae4fecad
coa3 <98691358+coa3@users.noreply.github.com>
2022-02-21 17:23:01 +0300

    Add files via upload

---
+++ freecodecamp.js
@@ -0,0 +1,11 @@
+let lastNameLength = 0;
+const lastName = "Lovelace";
+console.log(lastName.lenght);
+// Only change code below this line
+lastNameLength = lastName;
+
+const myArray = [
+    ["John", 23],
+    ["cat", 2]
+];
+let removedFromMyArray = myArray.pop();
