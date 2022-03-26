6d5792b60b86566abaf26f1dfc745b12e030b3d1
coa3 <98691358+coa3@users.noreply.github.com>
2022-03-26 19:21:54 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
-// for
-//while
+let out = document.querySelector('.out');
+console.log(out);
 
-let div = document.querySelectorAll('.one');
-console.log(div);
-//div.style.background = 'red';
-
-// for (let i = 0; i < div.length; i = i + 1) {
-//     console.log(div[i]);
-//     div[i].style.background = 'red';
-//     div[i].onclick = two;
+// for (let i = 0; i < 5; i++) {
+//     // i= 1
+//     for (let k = 0; k < 10; k++) {
+//         // k = 0
+//         out.innerHTML += '*';
+//     }
+//     out.innerHTML += '<br>';
 // }
 
-// function two() {
-//     console.log('work!!!');
-// }
-
-// let b = document.getElementsByClassName('one');
-// let c = document.getElementsByTagName('div');
-// console.log(b);
-// console.log(c);
-
-// for (let i = 0; i < b.length; i++) {
-//     b[i].style.border = '3px solid black';
-// } 
-
-document.querySelector('button').onclick = () => {
-    let r = document.querySelectorAll('input[type="radio"]');
-    console.log(r);
-    for (let i = 0; i < r.length; i++) {
-        if (r[i].checked) {
-            console.log(r[i].value);
-        }
+for (let i = 1; i < 10; i++) {
+    for (let k = 1; k < 10; k++) {
+        out.innerHTML += `${i}*${k}=${k * i}<br>`;
     }
+    out.innerHTML += '<hr>';
 }
-
-let out = '';
-for (let i = 0; i < 10; i++) {
-
-    if (i == 6) continue;
-    out += i + ' ';
-    // if (i == 6) break;
-}
-document.querySelector('#out').innerHTML = out;
