1494e00fa77147dab361ebb0a6a46850c45496e8
coa3 <coa3@mail.ru>
2022-06-29 14:09:47 +0300

    Add files via upload

--- unit_22.js
+++ unit_22.js
@@ -1,4 +1,4 @@
-//  Task 1
+// Task 1
 // Добавьте в код функции try catch так, чтобы вместо ошибки выводилось в out-1 цифра 1.
 
 
@@ -53,10 +53,10 @@ function t3() {
 document.querySelector('.b-3').onclick = t3;
 
 //  Task 4
-// Дана переменная a. В переменную делается push. Используя try catch отловите ошибки если они есть. Если ошибка вывести в out-4 число 0. Если не ошибка - то 
-// результирующий массив через пробел.
-let out4 = document.querySelector('.out');
+// Дана переменная a. В переменную делается push. Используя try catch отловите ошибки если они есть. Если ошибка вывести в out-4 число 0. Если 
+//не ошибка - то результирующий массив через пробел.
 
+let out4 = document.querySelector('.out-4');
 let a = [2, 3, 4];
 a = 5;
 
@@ -64,12 +64,9 @@ function t4() {
     try {
         a.push(7);
     } catch (err) {
-        if (err == true) {
-            out4.innerHTML = 0;
-        } else {
-            out4.innerHTML = a;
-        }
+        out4.innerHTML = '0';
     }
+    out4.innerHTML = a.join(' ');
 }
 document.querySelector('.b-4').onclick = t4;
 
@@ -78,8 +75,15 @@ document.querySelector('.b-4').onclick = t4;
 // Добавьте try, catch, finnaly так, чтобы в out-5 выводился 0 при ошибки. А в out-5-1 всегда выводилось слово 'finnaly';
 
 function t5() {
-    let p = document.querySelectorAll('p');
-    p.push(3);
+    let out5 = document.querySelector('.out-5');
+    try {
+        let p = document.querySelectorAll('p');
+        p.push(3);
+    } catch (err) {
+        out5.innerHTML = 0;
+    } finally {
+        document.querySelector('.out-5-1').innerHTML = 'finnaly';
+    }
 }
 
 document.querySelector('.b-5').onclick = t5;
