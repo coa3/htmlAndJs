27e9a21099bfea1b671ea0b6694875cda54ba2fe
coa3 <coa3@mail.ru>
2022-06-29 17:53:58 +0300

    Add files via upload

--- kurs.js
+++ kurs.js
@@ -29,6 +29,37 @@ let message = (age < 3) ? 'Здравствуй, малыш!' :
     (age < 100) ? 'Здравствуйте!' :
     'Какой необычный возраст!';
 
+/*   2 Основы ввода данных(update 2021 - 05 - 12)
+      3 Оператор If, else, switch case 
+      4 Работаем с формами: input, range, textarea
+      5 Циклы в JavaScript(часть 1)
+      6 Вложенные циклы в JavaScript(часть 2)
+      7 Функции и все о них
+      8 Цикл While, Do While
+      9 Работаем с DOM
+      10 Массивы в JavaScript
+      11 Добавление и удаление элементов в массиве, pop, push, splice
+      12 Двумерные массивы
+      13 Ассоциативный массив(объект) в JavaScript +
+      14 Практика по массивам - получаем прогноз погоды по API +
+      15 Set в JavaScript
+      16 Перебор массивов: for, for in, for of 
+      17 Методы массивов: map, filter(часть 1) 
+      18 Методы массивов: join, split, forEach(часть 2)
+      19 События мыши в JavaScript
+      20 События клавиатуры в JavaScript
+      21 Краткий обзор touch событий
+      22 Обрабатываем ошибки с помощью Try Catch
+      23 LocalStorage.Сохраняем все +
+      24 Немного теории - работа с POST, GET запросами +
+      25 AJAX - асинхронный JavaScript +
+      26 Учим FETCH на практике
+      27 Работаем с промисами(Promise) +
+      28 ООП в ES6
+      29 Бонусы - замыкания +
+      30 Бонус - Рекурсия
+      */
+
 //урок 2,3==========================================================================================================================================================
 document.getElementById('out').innerHTML = 2019; //обрашение к id элементу <div id="out">
 document.querySelector('#one').innerHTML = 777; // обрашение к id элементу <div id="out">
@@ -75,36 +106,7 @@ document.querySelector('#btn-2').onclick = (event) => {
         console.log(form.elements.two.value);
         console.log(form.elements.three.value);
 
-        /*   2 Основы ввода данных(update 2021 - 05 - 12)
-           3 Оператор If, else, switch case 
-           4 Работаем с формами: input, range, textarea
-           5 Циклы в JavaScript(часть 1)
-           6 Вложенные циклы в JavaScript(часть 2)
-           7 Функции и все о них
-           8 Цикл While, Do While
-           9 Работаем с DOM
-           10 Массивы в JavaScript
-           11 Добавление и удаление элементов в массиве, pop, push, splice
-           12 Двумерные массивы
-           13 Ассоциативный массив(объект) в JavaScript +
-           14 Практика по массивам - получаем прогноз погоды по API +
-           15 Set в JavaScript
-           16 Перебор массивов: for, for in, for of 
-           17 Методы массивов: map, filter(часть 1) 
-           18 Методы массивов: join, split, forEach(часть 2)
-           19 События мыши в JavaScript
-           20 События клавиатуры в JavaScript
-           21 Краткий обзор touch событий
-           22 Обрабатываем ошибки с помощью Try Catch
-           23 LocalStorage.Сохраняем все +
-           24 Немного теории - работа с POST, GET запросами +
-           25 AJAX - асинхронный JavaScript +
-           26 Учим FETCH на практике
-           27 Работаем с промисами(Promise) +
-           28 ООП в ES6
-           29 Бонусы - замыкания +
-           30 Бонус - Рекурсия
-           */
+
     }
     // урок 5 ЦИКЛЫ В JAVASCRIPT =======================================================================================
 document.querySelector('button').onclick = () => {
@@ -1256,3 +1258,162 @@ function myTouchMove(event) { // нажать и потянуть
     document.querySelector('.out-2').innerHTML += 'move ';
     return false;
 }
+/* Создайте блок div-2. Добавьте на него событие touchstart. Выведите в out-2 число срабатываний события. */
+let out22 = document.querySelector('.out-2');
+let schet = 0;
+
+function t2(event) {
+    schet++;
+    out22.innerHTML = schet;
+}
+document.querySelector('.div-2').addEventListener("touchstart", t2);
+
+/*  Создайте блок div-3_1 и div-3_2. Добавьте на них событие touchstart. Выведите в out-3 номер блока 1 или 2 на котором сработало событие. */
+let out3 = document.querySelector('.out-3');
+
+function t3(event) {
+    console.log(event);
+    let a = event.target;
+    if (a.classList.contains('div-3_1')) {
+        out3.innerHTML = 1;
+    }
+    if (a.classList.contains('div-3_2')) {
+        out3.innerHTML = 2;
+    }
+}
+document.querySelector('.div-3_1').addEventListener("touchstart", t3);
+document.querySelector('.div-3_2').addEventListener("touchstart", t3);
+
+/*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
+function t4() {
+    document.querySelector('.div-4').ontouchstart = t41;
+}
+
+function t41() {
+    document.querySelector('.out-4').innerHTML += 'touch';
+}
+document.querySelector('.b-4').onclick = t4;
+
+/*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
+
+function t6() {
+    document.querySelector('.out-6').innerHTML = 'touchend';
+}
+document.querySelector('.div-4').ontouchend = t6;
+/*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
+
+function t7() {
+    document.querySelector('.div-7').style.background = 'red';
+}
+document.querySelector('.div-7').addEventListener('touchstart', t7);
+
+/* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
+
+function t9(event) {
+    document.querySelector('.out-9').innerHTML = event.touches.length;
+}
+document.querySelector('.div-9').addEventListener('touchstart', t9);
+/*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
+let w = 75;
+
+function t10() {
+    document.querySelector('.div-10').style.width = w + 'px';
+    w++
+}
+document.querySelector('.div-10').addEventListener('touchmove', t10);
+
+/*  Мини проект. Ознакомьтесь с версткой в задании 12.
+<p>Изучите html код внутри div-12-wrapper.</p>
+<p>1. Добавьте на кнопку nex событие click, touch так, чтобы при событии запускалась функция nextFunction и активным становилось изображение следующее за выделенным 
+классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>2. Добавьте на кнопку prev событие click, touch так, чтобы при событии запускалась функция prevFunction и активным становилось изображение до выделенного классом 
+active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>3. Учтите краевые эффекты - когда мы доходим до конца или начала, то нажатие кнопки должно приводить к перемещению рамки в начало или конец изображений.</p>
+<p>4. Добавьте кнопку reset (функция resetFunction), нажатие которой сбрасывает активное изображение на нулевое. </p>
+<p>5. Добавьте во все действия следующее - в изображении img-12-max заменяется src на активную. Т.е. произошло событие - заменилась главная картинка.</p>
+*/
+let bigImg = document.querySelector('.img-12-max');
+const images = document.querySelectorAll('.img-12-min');
+let count = 0; // переменная, которая указывает на номер активного изображения в images
+
+const next = document.querySelectorAll('.next');
+next.onclick = nextFunction;
+
+const prev = document.querySelectorAll('.prev');
+prev.onclick = prevFunction;
+
+function nextFunction() {
+    if (count + 1 < images.length) {
+        count++
+    } else {
+        count = 0;
+    }
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.remove('active-img');
+    }
+    images[count].classList.add('active-img');
+    bigImg.src = images[count].src
+}
+
+function prevFunction() {
+    if (count - 1 >= 0) {
+        count--;
+    } else {
+        count = images.length - 1;
+    }
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.remove('active-img');
+    }
+    images[count].classList.add('active-img');
+    bigImg.src = images[count].src
+
+}
+
+function resetFunction() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.remove('active-img');
+
+    }
+    count = 0;
+    images[count].classList.add('active-img');
+    bigImg.src = images[count].src
+}
+document.querySelector('.next').addEventListener('touchstart', nextFunction);
+document.querySelector('.prev').addEventListener('touchstart', prevFunction);
+document.querySelector('.reset').addEventListener('touchend', resetFunction);
+
+// UNIT 22. ОБРАБАТЫВАЕМ ОШИБКИ С ПОМОЩЬЮ TRY CATCH =============================================================================================================
+
+let a = 5;
+a = a * 2;
+try { // код который возможно ошибочный помещаем сюда
+    document.querySelector('.test').innerHTML = a; // !
+} catch (err) { // если в коде есть ошибка то сюда выведет то что в коде
+    alert('ошибки!!!!');
+    console.log(err);
+    console.log('1');
+} finally { // в любом случае выведет то что в коде
+    console.log('программа работает');
+}
+console.log(a);
+t1();
+
+function t1() {
+    console.log('hello');
+}
+// Добавьте try, catch, finnaly так, чтобы в out-5 выводился 0 при ошибки. А в out-5-1 всегда выводилось слово 'finnaly';
+
+function t5() {
+    let out5 = document.querySelector('.out-5');
+    try {
+        let p = document.querySelectorAll('p');
+        p.push(3);
+    } catch (err) {
+        out5.innerHTML = 0;
+    } finally {
+        document.querySelector('.out-5-1').innerHTML = 'finnaly';
+    }
+}
+
+document.querySelector('.b-5').onclick = t5;
+// UNIT 23. LOCALSTORAGE. СОХРАНЯЕМ ВСЕ=============================================================================================================================
