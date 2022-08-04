03845a8bd3418f407e38af9e0f92531ecbb6eaee
coa3 <coa3@mail.ru>
2022-08-04 15:41:06 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,58 +1,65 @@
-import React from 'react';
+/*import logo from './logo.svg';*/
 import './App.css';
-
-;
 
 const App = () => {
   return (
-        <div className='app-wrapper'>
-   <header className='header'>
-    <img src='https://us.123rf.com/450wm/nicemosaic/nicemosaic1604/nicemosaic160400375/55246514-lotus-flower-hand-drawn-vintage-decorative-design-element-vector-illustration-clip-art-ideal-for-car.jpg?ver=6'></img>
-   </header>
-   <nav className='nav'>
-    <div>
-      <a>Profile</a>
-      </div>
-      <div>
-      <a>Messages</a>
-    </div>
-    <div>
+    <div className='app-wrapper'>
+<header className='header'>
+             <img src='https://us.123rf.com/450wm/nicemosaic/nicemosaic1604/nicemosaic160400375/55246514-lotus-flower-hand-drawn-vintage-decorative-design-element-vector-illustration-clip-art-ideal-for-car.jpg?ver=6'></img>
+  </header>
+<nav className = 'nav'>
+ <div>
+    <a>Profile</a>
+  </div>
+<div>
+  <a>Messages</a>
+  </div>
+<div>
     <a>News</a>
-    </div>
-    <div>
-    <a>Music</a>
-    </div>
-    <div>
-    <a>Settings</a>
-    </div>
-    </nav>
-   <div className='content'>
-    
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc1U8oRKiIzORBr5ffj2NYSAnVKGBu5oi6_g&usqp=CAU'></img>
-    </div>
-    <div>
-      ava + description
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-7AouXf8CrWUJlDaqpzKYvyMLACjlRmYU1Q&usqp=CAU'></img>
-    </div>
-    <div>
-      My posts
-      <div>
-        New posts
+  </div>
+<div>
+  <a>Music</a>
+  </div>
+<div>
+  <a>Settings</a>
+  </div>
+</nav>
+<div className='content'>
+<div>
+<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-7AouXf8CrWUJlDaqpzKYvyMLACjlRmYU1Q&usqp=CAU' alt='2'></img>
+</div>
+  <div>
+        ava + discription
       </div>
+<div>
+        My posts
+        <div>
+ New post
+        </div>
       <div>
-        <div>
-          post 1
+          <div>
+            post 1
+          </div>
+         <div>
+            post 2
+          </div>
         </div>
-        <div>
-          post 2
         </div>
-      </div>
-    </div>
-    </div>
-  );
+   </div>       
+</div>      
+);
 }
 
-
-
-
 export default App;
+ 
+  
+  
+  
+  
+
+
+  
+
+    
+  
+
